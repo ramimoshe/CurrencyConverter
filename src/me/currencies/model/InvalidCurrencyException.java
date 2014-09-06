4e30a99da85bf6c9d747cb89a825159d99297c90
Rami <rms.rami.m@gmail.com>
2014-09-06 18:02:17 +0300

    add InvalidCurrencyException

---
+++ src/me/currencies/model/InvalidCurrencyException.java
@@ -0,0 +1,23 @@
+package me.currencies.model;
+
+
+/**
+ * Exception indicating that the currency value was invalid
+ */
+class InvalidCurrencyException extends IllegalArgumentException {
+    /**
+     * Constructs a <tt>InvalidCurrencyException</tt> with no detail message.
+     */
+    public InvalidCurrencyException() {
+    }
+
+    /**
+     * Constructs a <tt>InvalidCurrencyException</tt> with the specified detail
+     * message.
+     *
+     * @param message the detail message
+     */
+    public InvalidCurrencyException(String message) {
+        super(message);
+    }
+}
